38c5ed01862257cf61df4387a2f93abfe3f0f7d1
Srivatsan <srivsriv@cisco.com>
2025-03-16 13:48:50 +0530

    first commit

---
+++ App.js
@@ -0,0 +1,32 @@
+// const heading = React.createElement("h1", {
+//     id: "heading"
+// }, "Hello World from React!!");
+// const root = ReactDOM.createRoot(document.getElementById("root"));
+// root.render(heading);
+
+const parent = React.createElement(
+    "div", 
+    { id: "parent"}, 
+   [
+    React.createElement(
+        "div", 
+        { id: "child" }, 
+        [
+            React.createElement("h1", {}, "Hello from H1"),
+            React.createElement("h2", {}, "Hello from H2"),
+        ]
+    ),
+    React.createElement(
+        "div", 
+        { id: "child2" }, 
+        [
+            React.createElement("h1", {}, "Hello from H1"),
+            React.createElement("h2", {}, "Hello from H2"),
+        ]
+    )
+   ]
+);
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(parent);
+
+// to create a simple h1 and h2 siblings for two child divs require this much clumsy code that is why we need JSX.
